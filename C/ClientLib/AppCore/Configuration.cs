f5f4d83141df40a63af6ffabaeca59ce2a99bbeb
minuowa <939813822@qq.com>
2017-06-02 10:07:03 +0800

    1

---
+++ C/ClientLib/AppCore/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore {
+public class Configuration {
+    public RunMode runMode = RunMode.Editor;
+}
+}
---
+++ C/ClientLib/AppCore/Shell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore {
+public class Shell {
+    public Configuration config {
+        get {
+            return mConfig;
+        }
+        set {
+            mConfig = value;
+        }
+    }
+    private Configuration mConfig = new Configuration();
+    public void RestartApp() {
+    }
+    public void Close() {
+    }
+}
+}
