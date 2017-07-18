6c1ec233bc87b5fe4c2a2db71dde1d1a5505c493
nnoldman <1527053436@qq.com>
2017-07-18 19:30:33 +0800

    鏁寸悊

--- C/Client/Assets/Plugins/Creature/IMoveBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
-
-public interface IMoveBase {
-    void MoveTo(Vector3 targetPosition);
-}
--- C/Client/Assets/Plugins/Creature/RGBUnitBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public class RGBUnitBase {
-    protected RGBActor mActor;
-    protected RGBHUD mHUD;
-}
