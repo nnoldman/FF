216277a376e83aa399519050c50b019cbfff3881
nnoldman <1527053436@qq.com>
2017-07-24 10:24:53 +0800

    1

--- BuildCommand.bat
+++ BuildCommand.bat
@@ -1,3 +1,5 @@
-cd ./CS
-flatc -c -o ../S/include Command.fbs
-flatc -n -o ../C/Client/Assets/Scripts Command.fbs
+cd ../CS
+protoc  --cpp_out=../S/include Command.proto
+protoc  --csharp_out=../C/Client/Assets/Scripts Command.proto
+::flatc -c -o ../S/include Command.fbs OPCode.fbs
+::flatc -n -o ../C/Client/Assets/Scripts Command.fbs OPCode.fbs
