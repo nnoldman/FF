b48d2a0383059382baaae0ee54a5da31ac03ce3b
nnoldman <1527053436@qq.com>
2017-07-24 19:13:59 +0800

    鏁寸悊

--- BuildCommand.bat
+++ BuildCommand.bat
@@ -1,5 +1,3 @@
 cd ../CS
-protoc  --cpp_out=../S/include Command.proto
+protoc  --cpp_out=../S/PKG Command.proto
 protoc  --csharp_out=../C/Client/Assets/Scripts Command.proto
-::flatc -c -o ../S/include Command.fbs OPCode.fbs
-::flatc -n -o ../C/Client/Assets/Scripts Command.fbs OPCode.fbs
