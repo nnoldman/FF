39b55aaf657ffbb972ba07b6011ced7fcf304994
nnoldman <1527053436@qq.com>
2017-07-24 21:47:19 +0800

    鏁寸悊protobuf

--- BuildCommand.bat
+++ BuildCommand.bat
@@ -1,3 +1,7 @@
-cd ../CS
+cd CS
+::客户端使用protobuf-net
+::后端使用protobuf-cpp-3.0.0
 protoc  --cpp_out=../S/PKG Command.proto
-protoc  --csharp_out=../C/Client/Assets/Scripts Command.proto
+protoc  --cpp_out=../S/PKG Cmd.proto
+protogen  -i:Command.proto -o:../C/Client/Assets/Scripts/Command.cs 
+protogen  -i:Cmd.proto -o:../C/Client/Assets/Scripts/Cmd.cs 
